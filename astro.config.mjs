699f6717a79db0c4a44b25fb823490715f998616
Bharath M. Palavalli <bmp@sdf.org>
2025-12-19 02:36:56 +0530

    fix: lighthouse score to 98 100 100 100

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://tarkam.org",
   integrations: [tailwind({ configFile: "./tailwind.config.mjs" })],
